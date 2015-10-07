11651bcbdeb3e623edb0a86ee9e12062d43035e6
dmarklein <daniel.klein.dmk@gmail.com>
2015-10-07 11:34:49 -0500

    added initial file with some notes/todos

---
+++ PageRank.java
@@ -0,0 +1,11 @@
+/*
+/  TODO:
+/   -- write driver
+      -- see https://github.com/abij/hadoop-wiki-pageranking/blob/master/src/com/xebia/sandbox/hadoop/WikiPageRanking.java
+      -- driver needs to parse graph.txt and construct initial input file into mapreduce
+      -- each intermediate result should be written to its own file
+    -- write mapper
+      -- see https://github.com/abij/hadoop-wiki-pageranking/blob/master/src/com/xebia/sandbox/hadoop/job2/calculate/RankCalculateMapper.java
+    -- write reducer
+      -- see https://github.com/abij/hadoop-wiki-pageranking/blob/master/src/com/xebia/sandbox/hadoop/job2/calculate/RankCalculateReduce.java
+**/
