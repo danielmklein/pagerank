a450fdf632f2b5384816a9f128709675777de5d2
dmarklein <daniel.klein.dmk@gmail.com>
2015-10-10 11:10:32 -0500

    wrote some code... it might almost work.

--- PageRank.java
+++ PageRank.java
@@ -31,4 +31,89 @@
 / so, my driver needs to initially build an input file
 / containing lines of data such as:
 / "nid init_pagerank outlink1 outlink2 outlink3 ... outlinkn"
+////////////////////////////////////////////////////////
+/
+/ building and running:
+/  $ mkdir Gender_classes
+/ $ javac -classpath ${HADOOP_HOME}/hadoop-${HADOOP_VERSION}-core.jar -d PageRank_classes PageRank.java
+/ $ jar -cvf /home/hadoop/PageRank.jar -C PageRank_classes/ .
+/
+/ $ hadoop PageRank.jar PageRank input_path output_path
 **/
+
+import java.io.IOException;
+import java.util.*;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapred.*;
+import org.apache.hadoop.util.*;
+
+public class PageRank {
+
+    public static class MapClass extends MapReduceBase
+      implements Mapper<LongWritable, Text, Text, LongWritable> {
+
+      private Text word = new Text();
+
+      public void map(LongWritable key, Text value,
+                      OutputCollector<Text, LongWritable> output,
+                      Reporter reporter) throws IOException {
+        String line = value.toString();
+        List<String> fields = line.split();
+        String curNid = fields[0];
+        Long initPageRank = Long.parseLong(fields[1]);
+        Long portion = initPageRank / (fields.size() - 2);
+
+        for (String outlinkId : fields.subList(2, fields.size() - 1))
+        {
+          word.set(outlinkId);
+          output.collect(word, new LongWritable(portion));
+        }
+        // parse nid, init_pagerank, and outlinks from line
+        // for nid in outlinks:
+        //    emit(nid, init_pagerank/len(outlinks))
+
+        StringTokenizer itr = new StringTokenizer(line);
+        while (itr.hasMoreTokens()) {
+          word.set(itr.nextToken());
+          output.collect(word, one);
+        }
+      }
+    }
+
+    public static class Reduce extends MapReduceBase
+      implements Reducer<Text, IntWritable, Text, IntWritable> {
+
+      public void reduce(Text key, Iterator<LongWritable> values,
+                         OutputCollector<Text, LongWritable> output,
+                         Reporter reporter) throws IOException {
+        long sum = 0;
+        while (values.hasNext()) {
+          sum += values.next().get();
+        }
+        output.collect(key, new LongWritable(sum));
+      }
+    }
+
+    public static void main(String[] args) throws Exception {
+      JobConf conf = new JobConf(PageRank.class);
+      conf.setJobName("PageRank");
+      conf.setOutputKeyClass(Text.class);
+      conf.setOutputValueClass(LongWritable.class);
+      conf.setInputFormat(TextInputFormat.class);
+      conf.setOutputFormat(TextOutputFormat.class);
+      conf.setMapperClass(Map.class);
+
+      if (args.length != 2) {
+          System.out.println("Usage:");
+          System.out.println(" /path/to/input/files /path/to/output");
+          System.exit(1);
+      }
+
+      FileInputFormat.setInputPaths(conf, new Path(args[0]));
+      FileOutputFormat.setOutputPath(conf, new Path(args[1]));
+      JobClient.runJob(conf);
+    }
+}
